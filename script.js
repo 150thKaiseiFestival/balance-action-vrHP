e477b3128aabd44a318fe503128a49dbc9eb2974
150thKaiseiFestival <77156262+150thKaiseiFestival@users.noreply.github.com>
2021-09-18 00:23:03 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(function() {
+  var windowWidth = $(window).width();
+  if(480<=windowWidth){
+  $(window).scroll(function() {
+    var scroll = $(this).scrollTop();
+    var height = $("body").outerHeight(true);
+    var scpoint1 = (scroll/height)*100+5;
+    var scpoint2 = (scroll/height)*100-5;
+    if (scpoint1>0 && scpoint1<=80){
+      $(".hello").css("width",scpoint1+"%")
+    }
+    if (scpoint2>0 && scpoint2<=80){
+      $(".position").css("width",scpoint2+"%")
+   }
+   console.log(scpoint1);
+  });
+  }
+
+});
+    
